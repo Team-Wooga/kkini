b33ab1fd6ce3ae5aacaef89f2adfd10287335be5
SEB_Full_36기_서외구 <89363048+sud665@users.noreply.github.com>
2022-05-09 17:06:07 +0900

    [Front]_Design_리액트 타입스크립트 모듈 설치

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Wooga 채팅 서비스 준비중 입니다. </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
