94fbda52b1240c0bb1d529242816c903fad00ff9
ssankq <ssankq@gmail.com>
2022-05-09 17:51:57 +0900

    [Back]_Env_CORS 옵션 타입 설정

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,13 +7,15 @@ import roomsRouter from "./routes/rooms";
 dotenv.config();
 
 const app: Application = express();
-const corsOptions = {
+const corsOptions: cors.CorsOptions = {
   origin: ["http://localhost:3000"],
   methods: ["GET", "POST", "DELETE", "UPDATE", "PATCH", "OPTIONS"],
   credentials: true,
 };
-app.use(cors(corsOptions));
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
 app.use("/users", usersRouter);
 app.use("/rooms", roomsRouter);
 
